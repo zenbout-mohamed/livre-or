3501d542f9d51635b2fcf988e7cd628ed42fa530
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 14:11:14 +0200

    page commentaire , formulaire en cour de réalisation

--- commentaire.php
+++ commentaire.php
@@ -9,10 +9,25 @@ if(!isset($_SESSION['id'])){
 
 if($_SERVER["REQUEST_METHOD"]==="POST"){
     $commentaire = $_POST['commentaire'];
-    $id_user = $_POST['id_user']
+    $id_user = $_POST['id'];
 
+    $sql = "INSERT INTO commentaires (commentaire,id_utilisateur) VALUE (?,?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($commentaire, $id_user);
+
+    header("Location: livre-or.php");
+    exit;
 }
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
