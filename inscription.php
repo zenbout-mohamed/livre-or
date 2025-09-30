a4d0704d9bcc789b39bacef72221b134828ce10c
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 11:42:53 +0200

    réalisation_en_cour_2, page inscription.php

--- inscription.php
+++ inscription.php
@@ -6,6 +6,19 @@ if($_SERVER['REQUEST_METHOD']=== "POST"){
     $login = $_POST['login'];
     $user = $_POST['passwrod'];
     $pass = $_POST['confirm'];
+    if ($password === $confirm) {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+
+        $sql = "INSERT INTO utilisateurs (login,password) VALUE (?,?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$login,$hash]);
+
+        header("location: connexion.php");
+        exit;
+    }else{
+        $error = "Les mots de passes ne correspondent pas.";
+    }
 }
 
 
