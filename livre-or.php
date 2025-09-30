8090e1c9efea193966120401af5b81ba0721f426
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 14:23:54 +0200

    jointure interne réalisé

--- livre-or.php
+++ livre-or.php
@@ -2,7 +2,8 @@
 session_start();
 require_once "db.php";
 
-$sql = "SELECT commentaires.commentaire, ";
+$sql = "SELECT commentaires.commentaire, commentaires.date, utilisateurs.login FROM commentaires
+        JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id ORDER BY commentaires.date DESC";
 
 
 
