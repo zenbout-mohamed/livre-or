81a566c473357bfe18dadf448c255a53e15f7db2
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 12:02:36 +0200

    page de connexion en cour de réalisation

--- connexion.php
+++ connexion.php
@@ -6,12 +6,21 @@ if ($_SERVER["REQUEST_METHOD"]=== "POST") {
    $login = $_POST['login'];
    $password =$_POST['password'];
 
-   $sql = "SELECT * FROM utilisateurs WHERE login = ?";
-   $stmt = $pdo->prepare($sql)
+    $sql = "SELECT * FROM utilisateurs WHERE login = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['login']);
+    $user = $stmt->fetch();
 
+    if($user && password_verify($password, $user['password'])){
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['login'] = $user['login'];
 
+        header("Location: index.php");
+        exit();
+    }else{
+        $error ="E-mail ou mot de passe incorrect.";
+    }
 
-}else{
 
 }
 
