93748f26c4c64360c2a5589ddd9dd6e9f2e8b1dd
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 16:52:46 +0200

    formulaire réalisé

--- profil.php
+++ profil.php
@@ -39,8 +39,34 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute($_SESSION['id']);
 $user = $stmt->fetch();
 
-
-
-
-
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+    <main>
+        <h1>Modifier un profil</h1>
+        <?php if(!empty($message)) echo "<p>$message</p>"; ?>
+        <form method="post">
+            <label for="login">Nom d'utilisateur</label>
+            <input type="text" name ="login" placeholder="Nom d'utilisateur :">
+
+            <label for="password">Nouveau mot de Passe : </label>
+            <input type="text" name="password" placeholder="Entrez votre nouveau mot de passe : ">
+
+            <label for="confirm_pass">Confirmation nouveau mot de passe :</label>
+            <input type="text" name ="confirm_Pass" placeholder ="Confirmez votre nouveau mot de passe :">
+
+
+            <button type="submit">Mettre à jour</button>
+        </form>
+
+        <p><a href="index.php">Retour à l'accueil</a></p>
+    </main>
+</body>
+</html>
