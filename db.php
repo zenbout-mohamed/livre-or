8688633832cfd599ae453c7e00629543e16c9172
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 11:08:30 +0200

    correction des variable  et  dans 'try'

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $user ="root";
 $pass = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", user, pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die(" Erreur : " . $e->getMessage());
