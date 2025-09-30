f1637b0b93344e9fdac16324eb3f451104f6cc3b
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-30 14:15:38 +0200

    page commentaire réalisé avec formulaire

--- commentaire.php
+++ commentaire.php
@@ -25,9 +25,14 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Livre D'Or - Page-Connexion</title>
 </head>
 <body>
-    
+    <main>
+        <form action="" method="post">
+            <textarea name="commentaire" required></textarea>
+            <button type="submit">Publier</button>
+        </form>
+    </main>
 </body>
 </html>
