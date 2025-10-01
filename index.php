8fd751b27281a68f215d42c8b72738ad5f64e746
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-01 09:56:47 +0200

    modification du titre de la page d'accueil

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livre D'Or - Page-Inscription</title>
+    <title>Livre D'Or - Accueil</title>
 </head>
 <body>
     
