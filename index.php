939cb5940fdc556f71895e073e7c0cf9f9f7502f
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-01 09:55:44 +0200

    création du fichier index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livre D'Or - Page-Inscription</title>
+</head>
+<body>
+    
+</body>
+</html>
